dac4772e6ef667876e05d341ed8857f0702f63d5
Jose Morelos <Josemorelos55@yahoo.com>
2018-04-02 18:52:19 -0700

    Java-scrip

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var num = 8;
+var test = ("what variable am i?");
+var names = ("Jose,Martha,Joe,Omar");
+var text = '';
+var i;
+for(i=0; i<10; i++) {
+    text += console.log(i +'' +names[i]) +'<br>';
+}
